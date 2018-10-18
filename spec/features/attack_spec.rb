9ec69b335ce16d1934f3201d2a57b47bf6a01285
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-18 14:53:15 +0100

    Can see Player 1's hp. Player 2 can attack Player 2

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,7 +8,15 @@ feature 'Attack' do
   scenario 'reduces player HP by 10' do
     sign_in_and_play
     click_link 'Attack'
-    click_link 'OK'
+    click_button 'OK'
     expect(page).to have_content 'Mittens: 90HP'
+  end
+
+  scenario 'be attacked by player 2' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_button 'OK'
+    click_link 'Attack'
+    expect(page).to have_content 'Mittens attacked Dave'
   end
 end
--- spec/features/viewing_hit_points_spec.rb
+++ spec/features/viewing_hit_points_spec.rb
@@ -1,6 +1,11 @@
 feature 'Viewing HP' do
-  scenario 'should show player hit points' do
+  scenario 'should show player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Mittens: 100HP'
+  end
+
+  scenario 'should show player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Dave: 100HP'
   end
 end
