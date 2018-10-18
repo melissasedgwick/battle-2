adf2c3b38fe944e97b3cea32383c88516296ce2a
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-18 14:32:55 +0100

    Switching turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,25 @@
 class Game
 
-  attr_reader :player_1, :player_2
+  attr_reader :player_1, :player_2, :current_turn
 
   def initialize(player_1, player_2)
+    @players = [player_1, player_2]
     @player_1 = player_1
     @player_2 = player_2
+    @current_turn = player_1
   end
 
   def attack(player)
     player.reduce_hp
+  end
+
+  def switch_turns
+    @current_turn = opponent_of(current_turn)
+  end
+
+  private
+  def opponent_of(the_player)
+    @players.select { |player| player != the_player }.first
   end
 
 end
